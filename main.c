53f038d2ea76cf88cab4cc38a72c1c43b53ead9b
EduardoBirchal <104524053+EduardoBirchal@users.noreply.github.com>
2022-08-20 15:23:15 -0300

    Coloquei função aleatória

--- main.c
+++ main.c
@@ -9,27 +9,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <time.h>
 #include "grafos.h"
 
 
 /*Consts*/
 #define SUCCESS 0
 
+#define TAM_GRAFO 9
+#define NUM_FALHAS 9 // É bom fazer NUM_FALHAS ser bem próximo de TAM_GRAFO, pra ter um núnero bom de vértices pais
+
 
 /*Functs*/
 
 int main (int argc, char ** argv){
-
 	bool tmp;
-	grafo_t* grafo = criaGrafo(5);
+	grafo_t* grafo = criaGrafo(TAM_GRAFO); // Faz um grafo de tamanho TAM_GRAFO+1
 
-	tmp = criaAresta(grafo, 1, 3, 2);
-	tmp = criaAresta(grafo, 3, 2, 2);
-	tmp = criaAresta(grafo, 2, 4, 2);
-	tmp = criaAresta(grafo, 4, 0, 2);
+	srand(time(NULL));
+
+	encheGrafo(grafo, TAM_GRAFO+1, NUM_FALHAS, True);
 
 	imprimeGrafo(grafo);
 
-
+	encontraMenoresCaminhos(grafo);
 	return SUCCESS;
+
+	// FALTA FAZER: Checar se o número de conexões com o vértice 0 é apropriado (não pode ter um monte de vértice ligando nele, mas não pode ser impossível chegar nele)
 }
