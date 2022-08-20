efd031b9ec02326332d3622a670475ec0ed81d1a
EduardoBirchal <104524053+EduardoBirchal@users.noreply.github.com>
2022-08-20 15:22:50 -0300

    Coloquei função aleatória

--- grafos.h
+++ grafos.h
@@ -9,6 +9,17 @@
 #define True 1
 #define False 0
 
+#define SUCCESS 0
+#define PASSAR_DE_ANO 0
+
+#define BRANCO 0 //Antes da busca
+#define CINZA 1 //Vértice visitado
+#define PRETO 2 //Todos os vértices adjacentes visitados 
+
+#define INDICE_INICIAL 1
+
+#define MAX_PESO 10 // O máximo que o peso de uma aresta pode ter
+
 /*Types and structs*/
 typedef int bool;
 
@@ -52,3 +63,24 @@ bool criaAresta(grafo_t* grafo, int vi, int vf, int peso);
 
 //Imprime os vértices, suas adjacências e pesos
 void imprimeGrafo(grafo_t* grafo);
+
+// Cria uma matriz de altura alt e largura larg
+int** mat_cria(int** matriz, int alt, int larg);
+
+// Insere um número numa matriz. Se não for usar peso, coloca peso=1
+void mat_insere(int** matriz, int v1, int v2, bool orientada, int peso);
+
+// Imprime uma matriz
+void mat_imprime(int** matriz, int larg, int alt);
+
+// Libera uma matriz quadrada
+void mat_libera(int** matriz, int alt);
+
+// Conecta um vértice com todos os outros, com algumas 'falhas de conexão' aleatórias
+void completaGrafo(grafo_t* grafo, int countVertices, int indice, int **falhas);
+
+// Faz lAdj_completa com um grafo inteiro
+void encheGrafo(grafo_t* grafo, int tamanho, int numFalhas, bool orientada);
+
+//Encontra os menores caminhos possíveis das tarefas até passar de ano
+void encontraMenoresCaminhos(grafo_t* grafo);
